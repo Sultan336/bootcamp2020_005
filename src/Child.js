f8950a9101075647e8751a95a1e478d004674eee
Sultan <m.sultan338@gmail.com>
2020-08-28 20:08:44 +0500

    Update Child.js

--- src/Child.js
+++ src/Child.js
@@ -6,7 +6,7 @@ const Child = () => {
     console.log(counterValue)
     return (
         <div>
-            <h2>This is first Child using Counter CounterContext.</h2>
+            <h2>First Child using Counter Context</h2>
             <h3>Counter Value is: {counterValue[0]}</h3>
             <button onClick={() => { counterValue[1](++counterValue[0]) }}>Increment Context</button>
         </div>
